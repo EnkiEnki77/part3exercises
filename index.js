6fed6093f694dbfc570f61b1dc9e12464dba05de
Enki Layman <layman212@gmail.com>
2023-05-30 10:01:50 -0400

    implemented route to get all phone numbers from db

--- index.js
+++ index.js
@@ -1,42 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-
+require('dotenv').config()
+const Phonebook = require('./models/phoneBook')
 app.use(express.json())
 app.use(morgan('tiny'))
 morgan.token('data', (req, res) => JSON.stringify(req.body))
 app.use(express.static('build'))
 
-let db = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-app.get('/', (req, res) => {
-    console.log('Hello World')
-    res.send('Hello World')
-})
-
 app.get('/api/persons', (req, res) => {
-    res.json(db)
+    Phonebook.find({})
+    .then(numbers => res.json(numbers))
 })
 
 app.get('/api/persons/:id', (req, res) => {
---
+++ models/phoneBook.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGO_URI
+
+mongoose.connect(url)
+.then(res => console.log('connected to mongo'))
+.catch(err => console.log('error connecting to mongo', err))
+
+const phoneBookSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+phoneBookSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Phone number', phoneBookSchema)
